1808f82bc541ab32cfa5fadd015e79d0cae8a0cf
Hassan Shamim <hassan.has.you@gmail.com>
2018-05-11 21:41:39 -0700

    part 3

--- dags/exercise3.py
+++ dags/exercise3.py
@@ -48,6 +48,31 @@ def get_input_file():
 # all available parameter types are available here: http://luigi.readthedocs.io/en/stable/api/luigi.parameter.html
 #
 # Tip: you may want to use a new output file
+class SourceTextTask(luigi.ExternalTask):
+    path = luigi.Parameter()
+
+    def output(self):
+        return luigi.LocalTarget(self.path)
+
+class SubstitutionTask2(luigi.Task):
+    sub_group = luigi.ChoiceParameter(var_type=int, choices=[1625, 1288])
+
+
+    def requires(self):
+        return SourceTextTask(path=get_input_file())
+
+    def run(self):
+        subs = {1625: substitutions1625, 1288: substitutions1288}
+        chosen = subs[self.sub_group]
+        with self.input().open('r') as infile, self.output().open('w') as outfile:
+            text = infile.read()
+            for old, new in chosen.items():
+                text = text.replace(old, new)
+            outfile.write(text)
+            print(text)
+
+    def output(self):
+        return luigi.LocalTarget('/usr/local/luigi/output/exercise3.txt')
 
 
 if __name__ == "__main__":
